8e2d24f910d2d7fb38984afa4633c9423ca0f2b4
Alan Kusparmakov <alan.kusparmakov@gmail.com>
2026-09-13 17:14:22 -0700

    command send functions

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -14,12 +14,22 @@ Class: 		CPE-3160
 #include "stm32l4xx_hal_gpio.h"
 #include <stdint.h>
 
+/* PC1-8 = DB0-7 */
+void DB_set(uint8_t command) {
+	GPIOC->ODR &= ~(0b11111111 << GPIO_ODR_OD1_Pos);
+	GPIOC->ODR |= (command << GPIO_ODR_OD1_Pos);
+}
+
+void RS_set(bool val) {
+	GPIOC->ODR &= ~GPIO_ODR_OD0_Msk;
+	GPIOC->ODR |= (val << GPIO_ODR_OD0_Pos);
+}
+
 void gpio_init() {
 	return;
 }
 
 int main() {
-	
 
 	return 0;
 }
